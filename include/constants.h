8929a18ff57c4ba3ce8c483f0e9c812c7f760abd
Mingde Yin <mingdeyin@gmail.com>
2024-06-05 20:23:49 -0400

    change sail power

--- include/constants.h
+++ include/constants.h
@@ -8,7 +8,7 @@ const double r_earth = 6378e3; // m
 // PARAMS
 const double pi = 3.1415926535897932384;
 
-const double sail_p = 9.12e-6;
+const double sail_p = 4.56e-6;
 const double sail_sigma = 0.01;
 const double sail_eta = 0.85;
 
