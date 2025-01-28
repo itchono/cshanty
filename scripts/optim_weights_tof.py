438e96bcbc18707d837e910c199b70b066e8df38
Mingde Yin <mdsuper@hotmail.com>
2025-01-28 22:51:08 +0000

    command line

--- scripts/optim_weights_tof.py
+++ scripts/optim_weights_tof.py
@@ -5,33 +5,43 @@ from scipy.optimize import dual_annealing
 
 from cshanty import cases
 from cshanty.optimization_funcs import tof_wrt_all_obj
+import argparse
 
 if __name__ == "__main__":
-    cfgs = [cases.BENCHMARK_TRANSFER, cases.OGURI_CASE_E]
-    cfg_names = ["CASE_A", "CASE_E"]
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("case")
+    
+    args = parser.parse_args()
+    
+    cfgdict = {
+        "CASE_A": cases.BENCHMARK_TRANSFER,
+        "CASE_E": cases.OGURI_CASE_E
+    }
+    
+    cfg = cfgdict[args.case]
 
-    for cfg, cfg_name in zip(cfgs, cfg_names):
-        for sail_sigma in [0.002, 0.005, 0.01, 0.05, 1 / 540]:
-            cfg.sail_sigma = sail_sigma
-            cfg.t_span = (0, 1.5e8)
+    for sail_sigma in [0.05, 1 / 540 / 0.85]:
+        cfg.sail_sigma = sail_sigma
+        cfg.t_span = (0, 1.5e9)
 
-            sys.stdout = open(
-                f"optim_weights_tof_{cfg_name}_{sail_sigma:.4f}.txt", "w", buffering=1
-            )
+        sys.stdout = open(
+            f"optim_weights_tof_{args.case}_{sail_sigma:.4f}.txt", "w", buffering=1
+        )
 
-            x = dual_annealing(
-                tof_wrt_all_obj,
-                bounds=(
-                    (55, 90),
-                    (0.1, 10),
-                    (0.1, 10),
-                    (0.1, 10),
-                    (0.1, 10),
-                    (0.1, 10),
-                ),
-                args=(cfg,),
-                maxiter=200,
-                x0=np.array([60, 1, 1, 1, 1, 1]),
-            )
+        x = dual_annealing(
+            tof_wrt_all_obj,
+            bounds=(
+                (55, 90),
+                (0.1, 10),
+                (0.1, 10),
+                (0.1, 10),
+                (0.1, 10),
+                (0.1, 10),
+            ),
+            args=(cfg,),
+            maxiter=200,
+            x0=np.array([60, 1, 1, 1, 1, 1]),
+        )
 
-            print(x)
+        print(x)
